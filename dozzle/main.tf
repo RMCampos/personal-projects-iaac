3609a585b2db26ca719bbf1c1a18c07eb6f83517
Ricardo Campos <ricardompcampos@gmail.com>
2026-02-19 16:31:32 -0300

    chore: add missing headers for tf file

--- dozzle/main.tf
+++ dozzle/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0" 
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 resource "kubernetes_namespace_v1" "dozzle" {
   metadata { name = "dozzle" }
 }
